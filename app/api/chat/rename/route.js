ae06ba0a8180c497441c96787553004210ecd29c
Jarrett Ferris <ferriswrites@gmail.com>
2025-04-03 20:48:40 -0700

    implemented chat saving, renaming, and deleting, finishing touches

---
+++ app/api/chat/rename/route.js
@@ -0,0 +1,24 @@
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import connectDB from "@/config/db";
+
+export async function POST(req) {
+    try {
+        const { userId } = getAuth(req)
+
+        if (!userId) {
+            return NextResponse.json({ success: false, message: "User not authenticated", });
+        }
+
+        const {chatId, name} = await req.json();
+        // Connect to the database and update the chat name
+        await connectDB();
+        await Chat.findOneAndUpdate({_id: chatId, userId}, {name});
+
+        return NextResponse.json({ success: true, message: "Chat renamed" })
+
+    } catch (error) {
+        return NextResponse.json({ success: false, error: error.message });
+    }
+}
